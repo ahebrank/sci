6479c0d1b7ffb23d071eb99b910b2ec43db0a28b
Andy Hebrank <ahebrank@gmail.com>
2019-07-17 15:37:43 -0400

    refactor batching

--- src/Entity/StaticContent.php
+++ src/Entity/StaticContent.php
@@ -7,8 +7,6 @@ use Drupal\Core\Field\BaseFieldDefinition;
 use Drupal\Core\Entity\ContentEntityBase;
 use Drupal\Core\Entity\EntityChangedTrait;
 use Drupal\Core\Entity\EntityTypeInterface;
-use Drupal\file\Entity\File;
-use Drupal\Core\Archiver\Zip;
 
 /**
  * Defines the Static content entity.
@@ -54,11 +52,11 @@ class StaticContent extends ContentEntityBase implements StaticContentInterface 
   use EntityChangedTrait;
 
   /**
-   * How many files to extract per set.
-   *
-   * @var int
+   * {@inheritdoc}
    */
-  protected $filesPerSet = 20;
+  public function label() {
+    return $this->getName();
+  }
 
   /**
    * {@inheritdoc}
@@ -127,57 +125,6 @@ class StaticContent extends ContentEntityBase implements StaticContentInterface 
   }
 
   /**
-   * Create the static zip.
-   *
-   * @param \Drupal\file\Entity\File $file
-   *   Uploaded (zip) file.
-   */
-  public function saveZip(File $file) {
-    // @TODO: inject
-    $real_path = \Drupal::service('file_system')->realpath($file->getFileUri());
-    $zip = new Zip($real_path);
-    if ($zip) {
-      $zip_files = $zip->listContents();
-
-      // Attempt to find an index.html file.
-      $index_path = "";
-      foreach ($zip_files as $file) {
-        if (strpos($file, 'index.html')) {
-          $index_path = $file;
-          break;
-        }
-      }
-      // Base URI is a hash of all the files in the archive.
-      $base_uri = "public://static/" . md5(implode('|', $zip_files));
-
-      // Set new path info.
-      $this->set('base_uri', $base_uri);
-      $url = $base_uri . '/' . $index_path;
-      $this->set('url', $url);
-      $this->save();
-
-      // Remove any file already there.
-      self::cleanOldContent($this->getBaseUri());
-
-      $sets = $this->splitFiles($zip_files);
-      $set_count = count($sets);
-      foreach ($sets as $set) {
-        $ops[] = [
-          '\Drupal\sci\Entity\StaticContent::extractSet',
-          [$zip, $base_uri, $set, $set_count],
-        ];
-      }
-      batch_set([
-        'title' => t('Extracting zip files'),
-        'operations' => $ops,
-        'finished' => '\Drupal\sci\Entity\StaticContent::batchFinished',
-      ]);
-      return TRUE;
-    }
-    return FALSE;
-  }
-
-  /**
    * {@inheritdoc}
    */
   public static function baseFieldDefinitions(EntityTypeInterface $entity_type) {
@@ -215,7 +162,7 @@ class StaticContent extends ContentEntityBase implements StaticContentInterface 
     $fields['width'] = BaseFieldDefinition::create('string')
       ->setLabel(t('Iframe width'))
       ->setDescription(t('Width of iframe element, in a valid attribute format (px or %).'))
-      ->setDefaultValue('')
+      ->setDefaultValue('100%')
       ->setDisplayOptions('form', [
         'type' => 'string_textfield',
         'weight' => -4,
@@ -233,7 +180,7 @@ class StaticContent extends ContentEntityBase implements StaticContentInterface 
     $fields['autoheight'] = BaseFieldDefinition::create('boolean')
       ->setLabel(t('Autoheight'))
       ->setDescription(t('Attempt to make height of iframe the height of the content when content loaded.'))
-      ->setDefaultValue('')
+      ->setDefaultValue(TRUE)
       ->setDisplayOptions('form', [
         'type' => 'checkbox',
         'weight' => -4,
@@ -291,56 +238,6 @@ class StaticContent extends ContentEntityBase implements StaticContentInterface 
       }
     }
     rmdir(rtrim($path, '/'));
-  }
-
-  /**
-   * Divide up the files into sets.
-   */
-  protected function splitFiles($files) {
-    $sets = [];
-    for ($j = 0; $j < count($files); $j += $this->filesPerSet) {
-      if ($j == 0) {
-        $i = 0;
-        continue;
-      }
-      $sets[] = array_slice($files, $i, $this->filesPerSet);
-      $i = $j;
-    }
-    return $sets;
-  }
-
-  /**
-   * Extraction callback.
-   */
-  public static function extractSet($zip, $path, $files, $set_count, &$context) {
-    if (!isset($context['sandbox']['progress'])) {
-      $context['sandbox']['progress'] = 0;
-      $context['results'] = [];
-    }
-
-    $zip->extract($path, $files);
-
-    $context['results'] = array_merge($context['results'], $files);
-    $context['sandbox']['progress']++;
-
-    // Inform the batch engine that we are not finished,
-    // and provide an estimation of the completion level we reached.
-    if ($context['sandbox']['progress'] != $set_count) {
-      $context['finished'] = $context['sandbox']['progress'] / $set_count;
-    }
-  }
-
-  /**
-   * Batch finished callback.
-   */
-  public static function batchFinished($success, $results, $operations) {
-    if ($success) {
-      $message = count($results) . ' ' . t('files extracted.');
-    }
-    else {
-      $message = t('There was an error during zip extraction.');
-    }
-    drupal_set_message($message);
   }
 
 }
--- src/Form/StaticContentForm.php
+++ src/Form/StaticContentForm.php
@@ -4,6 +4,9 @@ namespace Drupal\sci\Form;
 
 use Drupal\Core\Entity\ContentEntityForm;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\file\Entity\File;
+use Drupal\Core\Archiver\Zip;
+use Drupal\sci\Entity\StaticContent;
 
 /**
  * Form controller for Static content edit forms.
@@ -11,6 +14,13 @@ use Drupal\Core\Form\FormStateInterface;
  * @ingroup sci
  */
 class StaticContentForm extends ContentEntityForm {
+
+  /**
+   * How many files to extract from the zip per set.
+   *
+   * @var int
+   */
+  protected $filesPerSet = 10;
 
   /**
    * {@inheritdoc}
@@ -35,17 +45,150 @@ class StaticContentForm extends ContentEntityForm {
   /**
    * {@inheritdoc}
    */
-  public function save(array $form, FormStateInterface $form_state) {
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    parent::submitForm($form, $form_state);
     $entity = $this->entity;
-
-    parent::save($form, $form_state);
 
     $archive_fid = $form_state->getValue(['static_archive', '0']);
     // @TODO: inject this service.
     if (!empty($archive_fid) && $file = \Drupal::entityTypeManager()->getStorage('file')->load($archive_fid)) {
-      if ($entity->saveZip($file)) {
+      // Remove any file already there.
+      StaticContent::cleanOldContent($entity->getBaseUri());
+
+      if ($ops = $this->getZipOps($entity, $file)) {
+        $entity->save();
         $form_state->setRedirect('entity.static_content.canonical', ['static_content' => $entity->id()]);
+
+        batch_set([
+          'title' => $this->t('Extracting zip files'),
+          'operations' => $ops,
+          'finished' => '\Drupal\sci\Form\StaticContentForm::batchFinished',
+        ]);
       }
+    }
+  }
+
+  /**
+   * Create the static zip.
+   */
+  protected function getZipOps($entity, File $file) {
+    // @TODO: inject
+    $real_path = \Drupal::service('file_system')->realpath($file->getFileUri());
+    $zip = new Zip($real_path);
+    if ($zip) {
+      $zip_files = $zip->listContents();
+
+      // Attempt to find an index.html file.
+      $index_path = "";
+      foreach ($zip_files as $file) {
+        $basename = basename($file);
+        if ($basename === 'index.html') {
+          $index_path = $file;
+          break;
+        }
+      }
+
+      // Base URI is a hash of all the files in the archive.
+      $base_uri = "public://static/" . md5($entity->id() . '|' . implode('|', $zip_files));
+      if (!file_prepare_directory($base_uri, FILE_CREATE_DIRECTORY)) {
+        $message = $this->t('Unable to create directory: @dir', [
+          '@dir' => $base_uri,
+        ]);
+        \Drupal::messenger()
+          ->addError($message);
+        return FALSE;
+      }
+
+      $ops = [];
+      $sets = $this->splitFiles($zip_files);
+      $set_count = count($sets);
+      foreach ($sets as $set) {
+        $ops[] = [
+          '\Drupal\sci\Form\StaticContentForm::extractSet',
+          [$real_path, $base_uri, $set, $set_count],
+        ];
+      }
+      // $ops[] = [
+      //   '\Drupal\sci\Form\StaticContentForm::extractSet',
+      //   [$real_path, $base_uri, $zip_files, 1],
+      // ];
+
+      // Set new path info.
+      $entity->set('base_uri', $base_uri);
+      $url = $base_uri . '/' . $index_path;
+      $entity->set('url', $url);
+
+      return $ops;
+    }
+    return FALSE;
+  }
+
+  /**
+   * Divide up the files into sets.
+   */
+  protected function splitFiles($files) {
+    $sets = [];
+    $i = 0;
+    for ($j = 0; $i < count($files); $j += $this->filesPerSet) {
+      if ($j == 0) {
+        continue;
+      }
+      $sets[] = array_slice($files, $i, $this->filesPerSet);
+      $i = $j;
+    }
+    return $sets;
+  }
+
+  /**
+   * Extraction callback.
+   */
+  public static function extractSet($zip_path, $path, $files, $set_count, &$context) {
+    $zip = new Zip($zip_path);
+    if (!isset($context['sandbox']['progress'])) {
+      $context['sandbox']['progress'] = 0;
+      $context['results'] = [];
+    }
+
+    // Option to run in a single op.
+    if ($set_count == 1) {
+      $result = $zip->extract($path);
+    }
+    else {
+      $result = $zip->extract($path, $files);
+    }
+
+    if ($result) {
+      $context['results'] = array_merge($context['results'], array_values($files));
+      $context['message'] = 'Extracted ' . count($files) . ' files to ' . $path . ': ' . implode(', ', $files);
+    }
+    else {
+      $context['message'] = 'Unable to extract set.';
+    }
+    $context['sandbox']['progress']++;
+    // Inform the batch engine that we are not finished,
+    // and provide an estimation of the completion level we reached.
+    if ($context['sandbox']['progress'] != $set_count) {
+      $context['finished'] = $context['sandbox']['progress'] / $set_count;
+    }
+  }
+
+  /**
+   * Batch finished callback.
+   */
+  public static function batchFinished($success, $results, $operations) {
+    if ($success) {
+      $message = t("Zip extraction complete.");
+      \Drupal::messenger()
+        ->addStatus($message);
+    }
+    else {
+      $error_operation = reset($operations);
+      $message = t('An error occurred while processing %error_operation with arguments: @arguments', [
+        '%error_operation' => $error_operation[0],
+        '@arguments' => print_r($error_operation[1], TRUE),
+      ]);
+      \Drupal::messenger()
+        ->addError($message);
     }
   }
 
